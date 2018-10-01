d00dfe09d95d743325fe884aaff2f2681ae1f603
Juuso Meriläinen <juuso.merilainen@gofore.com>
2018-10-01 10:37:05 +0300

    Implement diphthong fixing (e.g. "ue" -> "uo")

--- sananmuunnin.py
+++ sananmuunnin.py
@@ -8,11 +8,35 @@ BACK_VOWELS = "aou"
 FRONT_VOWELS = "äöy"
 SWITCH_VOWELS = BACK_VOWELS + FRONT_VOWELS
 
+CLOSING_DIPHTHONGS = [
+    "ai",
+    "au",
+    "ei",
+    "eu",
+    "ey",
+    "iu",
+    "iy",
+    "oi",
+    "ou",
+    "ui",
+    "yi",
+    "äi",
+    "äy",
+    "öi",
+    "öy",
+]
+OPENING_DIPHTHONGS = ["ie", "uo", "yö"]
+DIPHTHONGS = OPENING_DIPHTHONGS + CLOSING_DIPHTHONGS
+
 
 def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def is_vowel(char):
+    return char in VOWELS
 
 
 def split(word):
@@ -25,7 +49,7 @@ def split(word):
     ('proo', 'sa')
     """
     for i, (prev, next_) in enumerate(pairwise(word)):
-        if prev in VOWELS and prev != next_:
+        if is_vowel(prev) and prev != next_:
             return word[:i + 1], word[i + 1:]
     return word, ""
 
@@ -78,13 +102,35 @@ def fix_vowel_counts(part_1, part_3):
     vowel_1 = part_1[-1]
     vowel_3 = part_3[-1]
     return (
-        f"{part_1[:-count_3]}{count_1 * vowel_1}",
-        f"{part_3[:-count_1]}{count_3 * vowel_3}",
+        part_1[:-count_3] + count_1 * vowel_1,
+        part_3[:-count_1] + count_3 * vowel_3,
     )
 
 
+def fix_diphthongs_and_join(old_beginning, new_beginning, ending):
+    """
+    >>> fix_diphthongs_and_join('vi', 'hu', 'eno')
+    'huono'
+    """
+    try:
+        old_chars = old_beginning[-1] + ending[0]
+        new_chars = new_beginning[-1] + ending[0]
+    except IndexError:
+        return new_beginning + ending
+
+    char = new_chars[0]
+
+    if new_chars in DIPHTHONGS or not all(map(is_vowel, new_chars)):
+        pass
+    elif old_chars in OPENING_DIPHTHONGS:
+        new_chars = char + {d[0]: d[1] for d in OPENING_DIPHTHONGS}[char]
+    elif old_chars in CLOSING_DIPHTHONGS:
+        new_chars = char + {d[0]: d[1] for d in CLOSING_DIPHTHONGS}[char]
+
+    return new_beginning[:-1] + new_chars + ending[1:]
+
+
 def process(word_1, word_2):
-    # FIXME fix erroneous diphthongs
     """
     >>> process('musta', 'naamio')
     ('nasta', 'muumio')
@@ -94,6 +140,8 @@ def process(word_1, word_2):
     ('kasti', 'tettavuus')
     >>> process('sata', 'prosenttia')
     ('prota', 'sasenttia')
+    >>> process('öööök', 'ää')
+    ('ääääk', 'öö')
     >>> process('vieno', 'huntti')
     ('huono', 'vintti')
     """
@@ -102,7 +150,10 @@ def process(word_1, word_2):
 
     part_1, part_3 = fix_vowel_counts(part_1, part_3)
 
-    word_1 = fix_vowel_harmony(part_1 + part_2)
-    word_2 = fix_vowel_harmony(part_3 + part_4)
+    word_1 = fix_diphthongs_and_join(part_3, part_1, part_2)
+    word_2 = fix_diphthongs_and_join(part_1, part_3, part_4)
+
+    word_1 = fix_vowel_harmony(word_1)
+    word_2 = fix_vowel_harmony(word_2)
 
     return word_1, word_2
